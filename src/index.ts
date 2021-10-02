50b68a058b34f536b4cdf1b298bf83512bf71afb
Marat Kadzov <mtkadzov@gmail.com>
2021-10-02 19:46:52 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import './index.scss';
+
+const content = document.querySelector('#content');
+(() => {
+  const bar = document.createElement('div');
+  bar.id = 'bar';
+  bar.contentEditable = 'true';
+  content.appendChild(bar);
+})();
