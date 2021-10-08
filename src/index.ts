325672e391660100acc4f2d37fdc27a7cab6c8fa
Marat Kadzov <mtkadzov@gmail.com>
2021-10-08 18:33:10 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,48 @@
+// check phone keyboard
+// tab styling
+// make save button glowing after typing in title
+
 import './index.scss';
 
 const content = document.querySelector('#content');
 
-const bar = document.createElement('div');
-bar.id = 'bar';
-content.appendChild(bar);
+const editor = document.createElement('div');
+editor.id = 'editor';
+content.append(editor);
 
-const plus = document.createElement('button');
-plus.className = 'fas fa-plus';
-bar.appendChild(plus);
+const title = document.createElement('textarea');
+title.className = 'input';
+title.placeholder = 'New task';
+title.rows = 1;
+editor.append(title);
 
-const text = document.createElement('input');
-text.type = 'text';
-text.placeholder = 'New task';
-bar.appendChild(text);
+const description = document.createElement('textarea');
+description.className = 'input';
+description.placeholder = 'Add details';
+description.rows = 1;
+editor.append(description);
+
+function changeHeight() {
+  this.style.height = 0;
+  this.style.height = `${this.scrollHeight}px`;
+}
+title.addEventListener('input', changeHeight);
+description.addEventListener('input', changeHeight);
+
+title.addEventListener('keydown', (e) => {
+  if (e.code === 'Enter') {
+    e.preventDefault();
+  }
+});
+
+const buttons = document.createElement('div');
+buttons.id = 'pane';
+editor.append(buttons);
+
+const save = document.createElement('button');
+save.append('Save');
+buttons.append(save);
+
+const cancel = document.createElement('button');
+cancel.append('Cancel');
+buttons.append(cancel);
