c9e860b906d135715b7bf143752f305c01dd2070
Marat Kadzov <mtkadzov@gmail.com>
2021-10-11 10:36:46 +0300

    add

--- src/editor.ts
+++ src/editor.ts
@@ -27,17 +27,23 @@ export default () => {
     }
   });
 
-  const pane = document.createElement('div');
-  pane.id = 'pane';
-  editor.append(pane);
-
   const save = document.createElement('button');
   save.append('Save');
-  pane.append(save);
+  editor.append(save);
+
+  title.addEventListener('input', () => {
+    if (title.textLength) {
+      save.style.backgroundColor = '#2683cf';
+      save.style.border = '0.1rem solid transparent';
+    } else {
+      save.style.backgroundColor = 'initial';
+      save.style.border = '0.1rem solid #6e6e6e';
+    }
+  });
 
   const cancel = document.createElement('button');
   cancel.append('Cancel');
-  pane.append(cancel);
+  editor.append(cancel);
 
   return editor;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-// check phone keyboard
-// tab styling
-// make save button glowing after typing in title
-// hamburger menu
-// drag and drop
-
 import './index.scss';
 import editor from './editor';
 
 const content = document.querySelector('#content');
 content.append(editor());
+
+// check phone keyboard
+// tab styling
+// hamburger menu
+// drag and drop
+
+// www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/todo-list
+// todoist.com/app/project/2275147841
+// to-do.live.com/tasks/today
+// //desktop.any.do/agenda
+// //keep.google.com/#home
