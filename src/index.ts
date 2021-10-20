63b491658fae3ca16d63be5b00ed7ac1b9fb5c1c
Marat Kadzov <mtkadzov@gmail.com>
2021-10-20 21:20:41 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,11 @@ content.append(editor());
 // www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/todo-list
 // todoist.com/app/project/2275147841
 // to-do.live.com/tasks/today
-// //desktop.any.do/agenda
-// //keep.google.com/#home
+// desktop.any.do/agenda
+// keep.google.com/#home
+
+// www.google.com/search?q=vim+vscode+tutorial&ei=LqNtYdqFBsaHwPAPv8CL4AI&ved=0ahUKEwja1Lj5stTzAhXGAxAIHT_gAiwQ4dUDCA4&uact=5&oq=vim+vscode+tutorial&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEOgcIABBHELADOgcIABCwAxBDOgcIABCxAxBDOgQIABBDOgYIABAHEB46CAgAEAgQBxAeOgQIABAeSgQIQRgAUMy8W1jayltgzs5baAJwAngAgAGpAYgByAmSAQMwLjiYAQCgAQHIAQrAAQE&sclient=gws-wiz
+// https://vim.rtorr.com/
+// https://devhints.io/vim
+// https://vimsheet.com/
+// https://gist.github.com/webdevel/4050973ffcb19edb07f72540c71ae38f
