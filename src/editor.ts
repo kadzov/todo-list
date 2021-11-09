3549fdb000a648a7ee2b39ea6f4b251cf7ed9303
Marat Kadzov <mtkadzov@gmail.com>
2021-11-09 20:15:32 +0300

    add

--- src/editor.ts
+++ src/editor.ts
@@ -23,22 +23,22 @@ export default () => {
   title.addEventListener('input', changeHeight);
   description.addEventListener('input', changeHeight);
 
-  const add = document.createElement('button');
-  add.append('Add');
-  add.id = 'add';
-  editor.append(add);
+  // const add = document.createElement('button');
+  // add.append('Add');
+  // add.id = 'add';
+  // editor.append(add);
 
-  title.addEventListener('keydown', (e) => {
-    if (e.code === 'Enter') {
-      e.preventDefault();
-      add.click();
-    }
-  });
+  // title.addEventListener('keydown', (e) => {
+  //   if (e.code === 'Enter') {
+  //     e.preventDefault();
+  //     add.click();
+  //   }
+  // });
 
-  const del = document.createElement('button');
-  del.append('Delete');
-  del.id = 'delete';
-  editor.append(del);
+  // const del = document.createElement('button');
+  // del.append('Delete');
+  // del.id = 'delete';
+  // editor.append(del);
 
   return editor;
 };
