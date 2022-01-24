b05f8c5e80ccc156b0ee00996e5b3fc19f5adc07
Marat Kadzov <mtkadzov@gmail.com>
2022-01-24 10:22:47 +0300

    add

--- src/calendar.ts
+++ src/calendar.ts
@@ -5,7 +5,18 @@ export default () => {
   const calendar = document.createElement('button');
   calendarDropdown.append(calendar);
   calendar.className = 'bi bi-calendar';
-  console.log(new Date().getFullYear());
 
-  return calendarDropdown
+  const calendarContent = document.createElement('div');
+  calendarDropdown.append(calendarContent);
+  calendarContent.className = 'calendar-content';
+
+  const date = new Date();
+
+  const month = document.createElement('div');
+  calendarContent.append(month);
+  month.textContent = `${date.toLocaleString('default', {
+    month: 'long',
+  })} ${date.getFullYear()}`;
+
+  return calendarDropdown;
 };
