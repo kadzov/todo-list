79204abebf654ec6a5f34c0f4437c368a45d35ef
Marat Kadzov <mtkadzov@gmail.com>
2022-01-27 12:42:45 +0300

    add

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,3 +1,5 @@
+import './calendar.scss';
+
 export default () => {
   const calendarDropdown = document.createElement('div');
   calendarDropdown.className = 'calendar-dropdown';
@@ -14,9 +16,39 @@ export default () => {
 
   const month = document.createElement('div');
   calendarContent.append(month);
+  month.className = 'month';
   month.textContent = `${date.toLocaleString('default', {
     month: 'long',
   })} ${date.getFullYear()}`;
+
+  const arrowLeft= document.createElement('button')
+  month.append(arrowLeft)
+  arrowLeft.className='bi bi-caret-left'
+
+  const arrowRight= document.createElement('button')
+  month.append(arrowRight)
+  arrowRight.className='bi bi-caret-right'
+
+  const days = document.createElement('div');
+  calendarContent.append(days);
+  days.className='days';
+
+  ['S', 'M', 'T', 'W', 'T', 'F', 'S'].forEach((i) => {
+    const day = document.createElement('div');
+    day.className = 'day';
+    day.textContent = i;
+    days.append(day);
+  });
+
+  Array.from(
+    Array(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()),
+    (_, i) => (i + 1).toString()
+  ).forEach((i) => {
+    const number = document.createElement('button');
+    number.className = 'number';
+    number.textContent = i;
+    days.append(number);
+  });
 
   return calendarDropdown;
 };
