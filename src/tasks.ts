c44618e783be13ec967a9b6bdd7884b7da61c119
Marat Kadzov <mtkadzov@gmail.com>
2022-02-09 13:28:00 +0300

    refactor

--- src/tasks.ts
+++ src/tasks.ts
@@ -8,108 +8,110 @@ export default () => {
   // add.addEventListener('click', () => {
   //   add.remove();
 
-    const task = template.content.cloneNode(true) as HTMLElement;
-    tasks.insertBefore(task, tasks.childNodes[0]);
+  const task = template.content.cloneNode(true) as HTMLElement;
+  tasks.insertBefore(task, tasks.childNodes[0]);
 
-    function completeTask() {
-      task.remove();
-      if (!tasks.contains(add)) {
-        tasks.insertBefore(add, tasks.childNodes[0]);
-      }
+  function completeTask() {
+    task.remove();
+    if (!tasks.contains(add)) {
+      tasks.insertBefore(add, tasks.childNodes[0]);
     }
+  }
 
-    const title = document.querySelector('.title') as HTMLInputElement;
-    // title.focus();
+  const circle = document.querySelector('.circle') as HTMLElement;
+  circle.onclick = completeTask;
 
-    const desc = document.querySelector('.desc') as HTMLInputElement;
-    desc.addEventListener('input', () => {
-      desc.style.height = '0';
-      desc.style.height = `${desc.scrollHeight}px`;
-    });
+  const title = document.querySelector('.title') as HTMLInputElement;
+  // title.focus();
 
-    const month = document.querySelector('.month') as HTMLElement;
-    const days = document.querySelector('.days') as HTMLElement;
-    const date = new Date();
+  const desc = document.querySelector('.desc') as HTMLInputElement;
+  desc.addEventListener('input', () => {
+    desc.style.height = '0';
+    desc.style.height = `${desc.scrollHeight}px`;
+  });
 
-    month.textContent = `${date.toLocaleString('default', {
-      month: 'long',
-    })} ${date.getFullYear()}`;
+  const month = document.querySelector('.month') as HTMLElement;
+  const days = document.querySelector('.days') as HTMLElement;
+  const date = new Date();
 
-    ['S', 'M', 'T', 'W', 'T', 'F', 'S'].forEach((i) => {
-      const day = document.createElement('div');
-      day.className = 'day';
-      day.textContent = i;
-      days.append(day);
-    });
+  month.textContent = `${date.toLocaleString('default', {
+    month: 'long',
+  })} ${date.getFullYear()}`;
 
-    Array.from(
-      Array(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()),
-      (_, i) => (i + 1).toString()
-    ).forEach((i) => {
-      const number = document.createElement('button');
-      number.className = 'number';
-      number.textContent = i;
-      days.append(number);
-    });
+  ['S', 'M', 'T', 'W', 'T', 'F', 'S'].forEach((i) => {
+    const day = document.createElement('div');
+    day.className = 'day';
+    day.textContent = i;
+    days.append(day);
+  });
 
-    let button: Element;
-    let content: Element;
+  Array.from(
+    Array(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()),
+    (_, i) => (i + 1).toString()
+  ).forEach((i) => {
+    const number = document.createElement('button');
+    number.className = 'number';
+    number.textContent = i;
+    days.append(number);
+  });
 
-    document.addEventListener('click', (e) => {
-      const element = e.target as Element;
-      const active = document.querySelector('.content.active');
+  let button: Element;
+  let content: Element;
 
-      if (element.matches('.dropdown > button')) {
-        if (active) {
-          content.classList.remove('active');
-        }
-        if (!active || button !== element) {
-          button = e.target as Element;
-          content = button.nextElementSibling as Element;
-          content.classList.toggle('active');
-        }
-      } else if (
-        active &&
-        !element.matches('.content :not(.number, .bi-flag-fill)')
-      ) {
+  document.addEventListener('click', (e) => {
+    const element = e.target as Element;
+    const active = document.querySelector('.content.active');
+
+    if (element.matches('.dropdown > button')) {
+      if (active) {
         content.classList.remove('active');
       }
-    });
-
-    // document.querySelectorAll('.bi-flag-fill').forEach((i) =>
-    //   i.addEventListener('click', (e) => {
-    //     const { color } = window.getComputedStyle(
-    //       e.target as Element,
-    //       'before'
-    //     );
-    //     circle.style.color = color;
-    //     check.style.color = color;
-    //   })
-    // );
-
-    const actions = document.querySelector('.actions') as HTMLElement;
-    const save = document.querySelector('.save') as HTMLElement;
-    const cancel = document.querySelector('.cancel') as HTMLElement;
-
-    save.addEventListener('click', () => {
-      if (title.value) {
-        actions.style.display = 'none';
-        if (!desc.value) {
-          title.style.height = '100%';
-          desc.style.display = 'none';
-        }
-        add.click();
+      if (!active || button !== element) {
+        button = e.target as Element;
+        content = button.nextElementSibling as Element;
+        content.classList.toggle('active');
       }
-    });
+    } else if (
+      active &&
+      !element.matches('.content :not(.number, .bi-flag-fill)')
+    ) {
+      content.classList.remove('active');
+    }
+  });
 
-    title.addEventListener('keydown', (e) => {
-      if (e.code === 'Enter') {
-        e.preventDefault();
-        save.click();
+  const check = document.querySelector('.bi-check') as HTMLElement;
+
+  document.querySelectorAll('.bi-flag-fill').forEach((i) =>
+    i.addEventListener('click', (e) => {
+      const { color } = window.getComputedStyle(e.target as Element, 'before');
+      circle.style.color = color;
+      check.style.color = color;
+    })
+  );
+
+  const extra = document.querySelector('.extra') as HTMLElement;
+  const save = document.querySelector('.save') as HTMLElement;
+  const cancel = document.querySelector('.cancel') as HTMLElement;
+
+  save.addEventListener('click', () => {
+    if (title.value) {
+      extra.style.display = 'none';
+      if (!desc.value) {
+        title.style.height = '100%';
+        desc.style.display = 'none';
       }
-    });
+      add.click();
+    }
+  });
 
-    cancel.onclick = completeTask;
+  title.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') {
+      e.preventDefault();
+      save.click();
+    }
+  });
+
+  cancel.onclick = completeTask;
   // });
 
   return tasks;
